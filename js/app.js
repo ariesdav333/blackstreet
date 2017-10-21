ff558402d101d9104e5d1e0469bb2f5d2bc29534
david <davidwilliams_canada@yahoo.com>
2017-10-21 14:43:00 +0100

    Commiting Change

--- js/app.js
+++ js/app.js
@@ -1,25 +1,32 @@
-angular.module('BlackApp', ['ngRoute', 'RouteControllers']);
+angular.module('BlackApp', ['ngRoute']);
  
 angular.module('BlackApp').config(function($locationProvider, $routeProvider) {
     $locationProvider.html5Mode(true);  // Enable href routing without hashes
  
     $routeProvider.when('/', {
         templateUrl: 'templates/home.html',
-        controller: 'HomeController'
+        
     })
 
-     .when('/about', {
+    .when('/about', {
         templateUrl: 'templates/about.html',
         //'TestimonialController'
     })
 
-     .when('/members', {
-        templateUrl: 'templates/members.html',
+    .when('/band', {
+        templateUrl: 'templates/band.html',
+        //'faqsController'
+    })
+
+    .when('/info', {
+        templateUrl: 'templates/info.html',
         // 'ContactusController'
     })
 
-    .when('/accounts/register', {
-        templateUrl: 'templates/register.html',
-        controller: 'RegisterController'
+    .when('/winclean', {
+        templateUrl: 'templates/winclean.html',
+        //controller: 'WincleanController'
     });
+
+
 });
